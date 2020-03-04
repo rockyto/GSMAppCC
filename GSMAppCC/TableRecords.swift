05b2e2727d4caa921e3f217488362fe179d07132
Rodrigo Sánchez <rsanchez@sma.com.mx>
2020-03-03 19:56:23 -0600

    Ajustes a la integración con SQLite 3

---
+++ GSMAppCC/TableRecords.swift
@@ -0,0 +1,73 @@
+//
+//  TableRecords.swift
+//  
+//
+//  Created by Rodrigo Sánchez on 03/03/20.
+//
+
+import Foundation
+import SQLite
+
+class TableRecords {
+    
+    static let shared = TableRecords()
+    
+    private let table = Table("recordsClientsCC")
+    
+    private let id = Expression<Int64>("idCCRecordsClients")
+    private let nombre = Expression<String>("clientName")
+    private let apellido = Expression<String>("clientLastName")
+    private let cumple = Expression<String>("clientBirthday")
+    private let genero = Expression<String>("clientGenre")
+    private let edad = Expression<Int>("clientAge")
+    
+    private let direccion = Expression<String>("clientAddress")
+    private let colonia = Expression<String>("clientSuburb")
+    
+    private let zip = Expression<String>("clientZipCode")
+    private let municipio = Expression<String>("clientTown")
+    private let ciudad = Expression<String>("clientCity")
+    private let mail = Expression<String>("clientMail")
+    private let cel = Expression<String>("clientCell")
+    private let id_cc = Expression<Int64>("ccID")
+    
+    private init(){
+        do{
+            if let conexion = Database.shared.conexion{
+                try conexion.run(table.create(temporary: false, ifNotExists: true, withoutRowid: false, block: { (t) in
+                                                t.column(self.id, primaryKey: true)
+                                                 t.column(self.nombre)
+                                                 t.column(self.apellido)
+                                                 t.column(self.cumple)
+                                                 t.column(self.genero)
+                                                 t.column(self.edad)
+                                                 t.column(self.direccion)
+                                                 t.column(self.colonia)
+                                                 t.column(self.zip)
+                                                 t.column(self.municipio)
+                                                 t.column(self.ciudad)
+                                                 t.column(self.mail)
+                                                 t.column(self.cel)
+                                                 t.column(self.id_cc)
+                }))
+                print("La tabla se creo correctamente")
+            }else{
+                print("La tabla no se creo")
+            }
+        } catch let error as NSError {
+            
+            print("La tabla no se creo", error)
+            
+        }
+    }
+    
+    func insertar(id: Int, nombre: String, apellido: String, cumple: String, genero: String, edad: Int, direccion: String, colonia: String, zip: String, municipio: String, ciudad: String, mail: String, cel: String, id_cc: Int){
+        do{
+            let insertar = table.insert(self.nombre <- nombre, self.apellido <- apellido, self.cumple <- cumple, self.genero <- genero, self.edad <- edad, self.direccion <- direccion, self.colonia <- colonia, self.zip <- zip, self.municipio <- municipio, self.ciudad <- ciudad, self.mail <- mail, self.cel <- cel, self.id_cc <- Int64(id_cc))
+            try Database.shared.conexion?.run(insertar)
+        } catch let error as NSError{
+            print("error al guardar", error)
+        }
+    }
+    
+}
